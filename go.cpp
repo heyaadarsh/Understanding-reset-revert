3779d3f0e7c1c4e17adc397b55a5f68fa09994f9
heyaadarsh <aadarsh0001.be21@chitkara@edu.in>
2022-06-02 15:18:28 +0530

    Reverting commit-2 | Go to Patna

--- go.cpp
+++ go.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main(){
 	cout<<"Go Delhi";
+<<<<<<< HEAD
 	cout<<"Go to Patna";
 	cout<<"Go to Mumbai"
+=======
+
+>>>>>>> parent of b220dd0 (Commit-2 | Go to Patna)
 return 0;
 }
